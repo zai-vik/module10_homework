1b149d2a357c80aa8fb55bea0a3caa7974a536ff
zai-vik <118923977+zai-vik@users.noreply.github.com>
2022-12-10 16:21:00 +0400

    Create type-of-variable.js

---
+++ type-of-variable.js
@@ -0,0 +1,15 @@
+const x = undefined;
+
+switch (typeof(x)) {
+  case 'number':
+    console.log('x - число');
+    break;
+  case 'string':
+    console.log('x - строка');
+    break;
+  case 'boolean':
+    console.log('x - логический тип');
+    break;
+  default:
+    console.log('Тип x не определён');
+}
